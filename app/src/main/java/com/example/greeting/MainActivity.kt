fec10b2ea39a34c6aaa7cca62a25a3a4ad4c0baa
prammmoe <ikhwanpramuditha05@gmail.com>
2023-10-09 11:36:49 +0700

    finalize greeting

--- app/src/main/java/com/example/greeting/MainActivity.kt
+++ app/src/main/java/com/example/greeting/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.greeting
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,8 +13,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -29,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(message = "Android", from = "Android")
+                    GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
                 }
             }
         }
@@ -40,15 +43,22 @@ class MainActivity : ComponentActivity() {
 // Best practice dari element Parent seperti Column adalah kita perlu untuk meneruskan parameter modifier ke elemen turunan dalam Composable
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
         Text(
             text = message,
             fontSize = 100.sp,
-            lineHeight = 116.sp
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
         )
         Text(
             text = from,
-            fontSize = 36.sp
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.End)
         )
     }
 }
@@ -58,6 +68,6 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday, Sam!", from = "From Emma")
+        GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
     }
 }
