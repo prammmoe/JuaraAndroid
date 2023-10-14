05768ce886b987a7d7805667d6194363cd6634ee
prammmoe <ikhwanpramuditha05@gmail.com>
2023-10-14 20:51:50 +0700

    fix: add background image

--- app/src/main/java/com/example/greeting/BusinessCard.kt
+++ app/src/main/java/com/example/greeting/BusinessCard.kt
@@ -4,9 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,9 +22,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.greeting.ui.theme.HappyBirthdayTheme
 
 class BusinessCard : ComponentActivity() {
@@ -41,19 +52,28 @@ class BusinessCard : ComponentActivity() {
 fun ComposeMain(name: String, jobTitle: String, modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.android_logo)
     Column {
+        Spacer(modifier = Modifier.padding(136.dp))
         Image(
             painter = image,
             contentDescription = null,
-            contentScale = ContentScale.Fit,
-            modifier = Modifier.align(Alignment.CenterHorizontally)
+            contentScale = ContentScale.Inside,
+            modifier = Modifier
+                .width(100.dp)
+                .background(Color.DarkGray)
+                .align(Alignment.CenterHorizontally)
         )
         Text(
             text = name,
+            textAlign = TextAlign.Center,
+            fontSize = 36.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
         )
         Text(
             text = jobTitle,
+            textAlign = TextAlign.Center,
             color = Color.Green,
             fontWeight = FontWeight.Bold,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
         )
     }
     Column {
